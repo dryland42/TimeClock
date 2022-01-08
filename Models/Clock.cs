1100e75f58579c0dcdf14baacc6201d5c5e0ae2e
dryland42 <rylan@rmrdevelopment.com>
2022-01-08 09:32:54 -0500

    employee class

---
+++ Models/Clock.cs
@@ -0,0 +1,9 @@
+namespace TimeClock.Models 
+{
+    public class Employee 
+    {
+        int Id { get; set; }
+        string LastName { get; set; }
+        string FirstName { get; set; }
+    }
+}
